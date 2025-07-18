f364db269369cb212757927ae253f670a7085b94
NOUMSI ULRICH STEVE <38357057+ulrich-sun@users.noreply.github.com>
2025-07-18 12:50:31 +0100

    gitop-mc 18

--- app/main.py
+++ app/main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 
 @app.get("/")
 async def read_main():
-    return {"msg": "DEBUT  DE LA MC GITOPS DATASCIENTEST"}
+    return {"msg": "DEBUT  DE LA MC GITOPS DATASCIENTEST du 18/07/2025"}
 
 
